d3f9b2e2bd93bb3fdde45f4d34c8b3e04f667474
rsbastos <rene.s.bastos@gmail.com>
2021-12-19 10:08:54 -0300

    criação da classe Application Start

---
+++ SpringBootEssentials/src/main/java/br/com/devdojo/awesome/start/ApplicationStart.java
@@ -0,0 +1,10 @@
+package br.com.devdojo.awesome.start;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args)
+    }
+}
