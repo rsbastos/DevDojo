10124b037295873d3a64ea5b87a540cf620f1e62
rsbastos <rene.s.bastos@gmail.com>
2021-12-20 18:00:05 -0300

    inclusão classe error e requisição Get id

--- springboot-essentials/src/main/java/br/com/devdojo/model/Student.java
+++ springboot-essentials/src/main/java/br/com/devdojo/model/Student.java
@@ -1,7 +1,22 @@
 package br.com.devdojo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class Student {
+    private int id;
     private String name;
+    public static List<Student> studentList;
+    static{
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this(name);
+        this.id = id;
+    }
 
     public Student(String name) {
         this.name = name;
@@ -10,11 +25,38 @@ public class Student {
     public Student() {
     }
 
+    private static void studentRepository() {
+        studentList = new ArrayList<>(asList(new Student(1, "Deku"), new Student( 2, "Todoroki")));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
