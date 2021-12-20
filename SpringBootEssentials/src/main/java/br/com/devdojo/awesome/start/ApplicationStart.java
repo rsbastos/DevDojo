2bc37a69fce2d0e7cd349dc88ef0b3b5f6585f19
rsbastos <rene.s.bastos@gmail.com>
2021-12-20 11:18:43 -0300

    criação do pacote endpoint

--- SpringBootEssentials/src/main/java/br/com/devdojo/awesome/start/ApplicationStart.java
+++ SpringBootEssentials/src/main/java/br/com/devdojo/awesome/start/ApplicationStart.java
@@ -1,8 +1,9 @@
 package br.com.devdojo.awesome.start;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-
+@EnableAutoConfiguration
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
