3cbd091b6e7164091868f50bc4fbb4c2596bbd55
rsbastos <rene.s.bastos@gmail.com>
2021-12-22 15:05:03 -0300

    Configurações_getter_setter_servidor rodando

--- springboot2.essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ springboot2.essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -9,4 +9,12 @@ public class Anime {
 
     public Anime() {
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- springboot2.essentials/src/main/java/academy/devdojo/springboot2/start/ApplicationStart.java
+++ springboot2.essentials/src/main/java/academy/devdojo/springboot2/start/ApplicationStart.java
@@ -2,8 +2,10 @@ package academy.devdojo.springboot2.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan(basePackages = "academy.devdojo.springboot2")
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
