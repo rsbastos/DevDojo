312a4f08c478dd51de3c8e84e531f07261a8d067
rsbastos <rene.s.bastos@gmail.com>
2021-12-22 18:02:54 -0300

    Criação de Repository e Service

--- springboot2.essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2.essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,8 +1,8 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +13,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
-    @GetMapping(path = "list")
+    @GetMapping
     public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+        return animeService.listAll();
     }
 }
