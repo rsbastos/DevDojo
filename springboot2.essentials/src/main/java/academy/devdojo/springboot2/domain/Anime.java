42e4865f8e7b78d8afa9d80741c865014e52e0af
rsbastos <rene.s.bastos@gmail.com>
2021-12-27 21:11:51 -0300

    teste de banco de dados pelo docker rodando

--- springboot2.essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ springboot2.essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,22 +1,24 @@
 package academy.devdojo.springboot2.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
 
 }
---
+++ springboot2.essentials/src/main/java/academy/devdojo/springboot2/requests/AnimePutRequestBody.java
@@ -0,0 +1,14 @@
+package academy.devdojo.springboot2.requests;
+
+import lombok.Data;
+
+@Data
+public class AnimePutRequestBody {
+    private Long id;
+    private String name;
+
+
+    }
+
+
+
