847dec7dbe5bda8edd4f85b1a86493a46fc1ae07
rsbastos <rene.s.bastos@gmail.com>
2022-01-03 12:14:19 -0300

    PUT

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -39,7 +39,12 @@ public class AnimeController {
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
         animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody Anime anime) {
+        animeService.replace(anime);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
