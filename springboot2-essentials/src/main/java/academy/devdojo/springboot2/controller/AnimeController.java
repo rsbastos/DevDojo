7aa8f9223e6b2a86358aa2ce654508859103dd4e
rsbastos <rene.s.bastos@gmail.com>
2022-01-03 09:44:13 -0300

    inclusão de service e domain

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,17 +1,28 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
-    @GetMapping(path = "list")
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
+
+    @GetMapping
     public List<Anime> list() {
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return animeService.listAll();
     }
 }
---
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -0,0 +1,14 @@
+package academy.devdojo.springboot2.service;
+
+import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AnimeService {
+//    private final AnimeRepository animeRepository;
+    public List<Anime> listAll() {
+        return List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+    }
+}
