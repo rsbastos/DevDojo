7e7328f8b5d121982f12e9745934b6c2f7f797e3
rsbastos <rene.s.bastos@gmail.com>
2022-01-04 17:54:14 -0300

    Rest Template

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -24,6 +24,10 @@ public class AnimeService {
         return animeRepository.findAll(pageable);
     }
 
+    public List<Anime> listAllNonPageable() {
+            return animeRepository.findAll();
+    }
+
     public List<Anime> findByName(String name) {
         return animeRepository.findByName(name);
     }
@@ -50,4 +54,6 @@ public class AnimeService {
                 .build();
         animeRepository.save(anime);
     }
+
+
 }
