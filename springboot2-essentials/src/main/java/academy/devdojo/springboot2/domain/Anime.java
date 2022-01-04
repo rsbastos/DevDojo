b1871ee964a643841e2f31f56da66030e9eef40e
rsbastos <rene.s.bastos@gmail.com>
2022-01-04 11:11:15 -0300

    Handler Para Validação de Campos

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,5 +21,4 @@ public class Anime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
 }
