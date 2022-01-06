117373f22ce5e149d71f9d23e5352bda545adb6f
rsbastos <rene.s.bastos@gmail.com>
2022-01-06 14:00:47 -0300

    Testes Spring Data JPA - Exceções

--- springboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ springboot2-essentials/src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
@@ -20,5 +21,6 @@ public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "The anime name cannot be empty or null")
     private String name;
 }
